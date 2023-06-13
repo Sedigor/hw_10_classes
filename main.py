6340046e40e9c92031613b472d86d84fb18188f7
Ihor Siedielnikov <sisemail92@gmail.com>
2023-06-13 19:49:35 +0300

    commit 1

---
+++ main.py
@@ -0,0 +1,34 @@
+from collections import UserDict
+
+class Field:
+    pass
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Record:
+    def __init__(self, name):
+        self.name = name
+        self.phones = []
+        
+    def name(self):
+        return self.name
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone == old_phone:
+                phone.value = new_phone
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        key = record.name()
+        self.data[key] = record
